e64f614176b0b5f9485706e5570977b022cc49c0
Anhle65 <levananh235813@gmail.com>
2023-12-06 12:06:28 +1300

    Abstract class/method and its application

---
+++ AbstractClass/Shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AbstractClass
+{   
+    //abstract method must inside an abstract class
+    public abstract class Shape
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        // abstract is only declared 
+        public abstract void Draw();
+        public void Copy()
+        {
+            Console.WriteLine("Copy shape into clipboard");
+        }
+    }
+}
