7c420e7f86ef59dd1e92fdf337e34e170c095a7a
Anhle65 <levananh235813@gmail.com>
2023-12-05 15:46:39 +1300

    Create Stack class

---
+++ Exercises-Inheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Exercises_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            try
+            {
+                stack.Push(1);
+                stack.Push("");
+                stack.Push("Vanh");
+                stack.Push(2);
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+            }
+             catch (InvalidOperationException ex)
+            {
+                Debug.WriteLine($"Error: {ex}");
+            }
+        }
+    }
+}
---
+++ Exercises-Inheritance/Stack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Exercises_Inheritance
+{
+    public class Stack
+    {
+        public List<object> list = new List<object>();
+        public void Push(object data)
+        {
+            list.Add(data);
+            if (data == null)
+            {
+                throw new InvalidOperationException("Input is a null");
+            }
+        }
+        public object Pop()
+        {   
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+            else
+            {   
+                object last_item = list[list.Count - 1];
+                list.RemoveAt(list.Count - 1);
+                return last_item;
+            }
+        }
+        public void Clear()
+        {
+            list.Clear();
+        }
+    }
+}
