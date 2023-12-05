2dcb28c83868557d8e9a0e0af5e0618a03ec2768
Anhle65 <levananh235813@gmail.com>
2023-12-06 11:34:23 +1300

    Override method

---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MethodOverriding
+{   
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var shapes = new List<Shape>();
+            shapes.Add(new Circle());
+            shapes.Add(new Triangle());
+            
+            var canvas = new Canvas();
+            
+            canvas.DrawShapes(shapes);
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MethodOverriding/Shape.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MethodOverriding
+{   
+    // can override because Draw() method is set to virtual 
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a circle");
+        }
+
+    }
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a triangle");
+        }
+    }
+    public class Shape
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public Position position { get; set; }
+        public ShapeType Type { get; set; }
+        public virtual void Draw()
+        {
+
+        }
+    }
+}
