99241dd1224652187d714a9980e7e1975e5b6018
Anhle65 <levananh235813@gmail.com>
2023-12-06 22:17:38 +1300

    Exercise creating DbConnection

---
+++ Exercise-Polymorphism/DbConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise_Polymorphism
+{
+    public abstract class DbConnection
+    {
+        private string _connectionString;
+        public TimeSpan Timeout { get; set; }
+        public DbConnection()
+        {
+
+        }
+        public DbConnection(string ConnectionString)
+        {
+            _connectionString = ConnectionString;
+            if (_connectionString == null || _connectionString == " ")
+            {
+                throw new ArgumentNullException("The connection can't be null or an empty string");
+            }
+        }
+        public abstract void Opening();
+        public abstract void Closing();
+    }
+}
